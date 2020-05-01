8813c36553338d9b9672746d4227092dbcce210d
Carlos Gutierrez <elimperiodeloefimero@gmail.com>
2020-04-30 20:28:39 -0500

    Se agregan referencias al DOM de imputs gestionados por el usuario

--- js/main.js
+++ js/main.js
@@ -1,2 +1,7 @@
 const compose = (...functions) => data =>
   functions.reduceRight((value, func) => func(value), data)
+
+const DESCRIPTION = document.querySelector('#description')
+const CALORIES = document.querySelector('#calories')
+const CARBS = document.querySelector('#carbs')
+const PROTEIN = document.querySelector('#protein')

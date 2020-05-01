404a893b26dc73e4e7cfe29989f620f5d35c69d3
Carlos Gutierrez <elimperiodeloefimero@gmail.com>
2020-04-30 21:51:08 -0500

    Se agregan validaciones para evitar enviar campos vacios

--- js/main.js
+++ js/main.js
@@ -5,3 +5,22 @@ const DESCRIPTION = document.querySelector('#description')
 const CALORIES = document.querySelector('#calories')
 const CARBS = document.querySelector('#carbs')
 const PROTEIN = document.querySelector('#protein')
+
+DESCRIPTION.onkeypress = () => DESCRIPTION.classList.remove('is-invalid')
+CALORIES.addEventListener('keypress', () => CALORIES.classList.remove('is-invalid'))
+CARBS.onkeypress = () => CARBS.classList.remove('is-invalid')
+PROTEIN.onkeypress = () => PROTEIN.classList.remove('is-invalid')
+
+const validateInputs = () => {
+  !DESCRIPTION.value ? DESCRIPTION.classList.add('is-invalid') : ''
+  !CALORIES.value ? CALORIES.classList.add('is-invalid') : ''
+  !CARBS.value ? CARBS.classList.add('is-invalid') : ''
+  !PROTEIN.value ? PROTEIN.classList.add('is-invalid') : ''
+
+  if (DESCRIPTION.value &&
+    CALORIES.value &&
+    CARBS.value &&
+    PROTEIN.value) {
+    console.log('ok! next funcionality!')
+  }
+}
